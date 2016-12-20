8ab0d2b02af8e77a4a838da43f64293aa03b8404
Davor Prugovečki <davor.prugovecki@gmail.com>
2016-12-20 09:09:19 +0100

    Stable version build

--- ChangeVersions.bat
+++ ChangeVersions.bat
@@ -1,8 +1,8 @@
 @SETLOCAL
 @SETLOCAL ENABLEDELAYEDEXPANSION
 @REM //////////////////////////////////////////////////////
-SET BuildVersion=1.2.0
-SET PrereleaseVersion=auto
+SET BuildVersion=1.0.0
+SET PrereleaseVersion=
 @REM SET PrereleaseVersion TO EMPTY VALUE FOR THE OFFICIAL RELEASE.
 @REM SET PrereleaseVersion TO "auto" FOR AUTOMATIC PRERELEASE NAME "alpha<date and time><last commit hash>"
 @REM \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
