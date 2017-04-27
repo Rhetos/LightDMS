93c818cbca7dcc9e5dedafc5393f00d2504e557e
Davor Prugovečki <davor.prugovecki@gmail.com>
2017-04-27 14:20:54 +0200

    Release verzije.

--- ChangeVersions.bat
+++ ChangeVersions.bat
@@ -1,7 +1,7 @@
 @SETLOCAL
 @SETLOCAL ENABLEDELAYEDEXPANSION
 @REM //////////////////////////////////////////////////////
-SET BuildVersion=1.2.6
+SET BuildVersion=1.2.7
 SET PrereleaseVersion=
 @REM SET PrereleaseVersion TO EMPTY VALUE FOR THE OFFICIAL RELEASE.
 @REM SET PrereleaseVersion TO "auto" FOR AUTOMATIC PRERELEASE NAME "alpha<date and time><last commit hash>"
