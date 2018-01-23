b83e0ecbe3f63d468303ad01242f14e93951a6cb
Hrvoje Milas <hrvoje.milas@omega-software.hr>
2018-01-23 12:58:06 +0100

    Dizanje verzije

--- Plugins/Rhetos.LightDMS/Properties/AssemblyInfo.cs
+++ Plugins/Rhetos.LightDMS/Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.9")]
 [assembly: AssemblyFileVersion("1.2.9")]
-[assembly: AssemblyInformationalVersion("1.2.9-dev")]
+[assembly: AssemblyInformationalVersion("1.2.9")]
