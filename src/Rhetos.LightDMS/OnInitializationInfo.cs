d262e83a7115dd518a4def932fe559e6ec01ac33
Ivan Barbančić <ibarbancic@omega-software.hr>
2021-02-22 18:30:51 +0100

    Removed usage of OnInitialization concept.

--- src/Rhetos.LightDMS/OnInitializationInfo.cs
+++ src/Rhetos.LightDMS/OnInitializationInfo.cs
@@ -18,12 +18,10 @@
 */
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Rhetos.Dsl;
 using System.ComponentModel.Composition;
 using Rhetos.Dsl.DefaultConcepts;
+using Rhetos.Dom.DefaultConcepts;
 
 namespace Rhetos.LightDMS
 {
