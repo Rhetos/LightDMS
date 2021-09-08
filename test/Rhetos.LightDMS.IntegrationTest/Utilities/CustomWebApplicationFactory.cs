4a4f038e4b41f24ea74acda8ca566c16f33e98cf
Quoc Ngo <quoc99@gmail.com>
2021-09-08 13:15:46 +0700

    Enable DI overrides for CustomerWebApplicationFactory.

--- test/Rhetos.LightDMS.IntegrationTest/Utilities/CustomWebApplicationFactory.cs
+++ test/Rhetos.LightDMS.IntegrationTest/Utilities/CustomWebApplicationFactory.cs
@@ -2,22 +2,38 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 
 namespace Rhetos.LightDMS.IntegrationTest
 {
     public class CustomWebApplicationFactory<TStartup>
         : WebApplicationFactory<TStartup> where TStartup : class
     {
+        private readonly Action<IServiceCollection> _serviceBuilder;
+
+        public CustomWebApplicationFactory() 
+        {
+            _serviceBuilder = (services) => { };
+        }
+
+        public CustomWebApplicationFactory(Action<IServiceCollection> serviceBuilder)
+        {
+            _serviceBuilder = serviceBuilder;
+        }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services =>
-                services.AddRhetosHost(ConfigureRhetos)
-            );
+            {
+                services.AddRhetosHost(ConfigureRhetos);
+                _serviceBuilder.Invoke(services);
+            });
         }
 
         private void ConfigureRhetos(IServiceProvider serviceProvider, IRhetosHostBuilder rhetosHostBuilder)
         {
-            rhetosHostBuilder.UseRootFolder(@"..\..\..\..\Rhetos.LightDMS.TestApp\bin\Debug\net5.0");
+            var rootPath = Path.Combine("..", "..", "..", "..", "Rhetos.LightDMS.TestApp", "bin", "Debug", "net5.0");
+            rhetosHostBuilder.UseRootFolder(rootPath);
         }
     }
 }
