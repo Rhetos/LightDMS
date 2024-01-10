e789ad7ae0ebc981eb013d1c9313be035d71f1ce
Bojan Antolović <bojan.antolovic@gmail.com>
2024-01-10 15:16:34 +0100

    Reorganized source folders.

--- Clean.bat
+++ Clean.bat
@@ -3,3 +3,7 @@
 @REM Question mark is here to prevent an issue with 'dir' not listing the subfolders if there is a folder with the same name in the project root folder.
 @FOR /F "delims=" %%i IN ('dir TestResult? /s/b/ad') DO DEL /F/S/Q "%%i" >nul & RD /S/Q "%%i"
 @REM Empty line, to prevent this script from returning an error code if there are no folders found in the previous command.
+
+IF EXIST "test\Rhetos.LightDMS.TestApp\AfterDeploy" RD /S/Q "test\Rhetos.LightDMS.TestApp\AfterDeploy"
+IF EXIST "test\Rhetos.LightDMS.TestApp\DslScripts\LightDMS" RD /S/Q "test\Rhetos.LightDMS.TestApp\DslScripts\LightDMS"
+IF EXIST "test\Rhetos.LightDMS.TestApp\DataMigration" RD /S/Q "test\Rhetos.LightDMS.TestApp\DataMigration"
